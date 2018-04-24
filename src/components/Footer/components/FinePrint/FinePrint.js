187bbb260ec603e3ae6b299f90bd26e9ffa0265a
Gregory Varias <gregvarias@gmail.com>
2018-04-24 01:30:13 -0700

    feat(footer): add FinePrint and Menu to Footer

---
+++ src/components/Footer/components/FinePrint/FinePrint.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import styles from './fine-print.module.less'
+
+const FinePrint = () => (
+  <div className={styles.container}>
+    <p>&copy; 2017 Empire Asset Company | 29 Broadway, 12th Floor, New York, NY 10006 | 888-417-7777 | <a href='#'>info@empiream.com</a></p>
+    <p>Member FINRA / SIPC | <a href='#'>FINRA's BrokerCheck</a> | <a href='#'>Client's Rights &amp; Protections</a></p>
+  </div>
+)
+
+FinePrint.displayName = 'FinePrint'
+export default FinePrint
---
+++ src/components/Footer/components/FooterMenu/FooterMenu.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+import styles from './footer-menu.module.less'
+
+const FooterMenu = () => (
+  <ul className={styles.container}>
+    <li>
+      <div>
+        <Link className={styles.main} to='#'>About Empire</Link>
+        <Link className={styles.sub} to='#'>Our Philosophy</Link>
+        <Link className={styles.sub} to='#'>Relationships Matter</Link>
+      </div>
+    </li>
+    <li>
+      <div>
+        <Link className={styles.main} to='#'>Brokerage</Link>
+        <Link className={styles.sub} to='#'>Transactional Brokerage</Link>
+        <Link className={styles.sub} to='#'>Institutional Research, Sales &amp; Trading</Link>
+      </div>
+    </li>
+    <li>
+      <div>
+        <Link className={styles.main} to='#'>Asset Management</Link>
+        <Link className={styles.sub} to='#'>Financial Planning</Link>
+      </div>
+    </li>
+    <li>
+      <div>
+        <Link className={styles.main} to='#'>Investment Banking</Link>
+        <Link className={styles.sub} to='#'>Capital Markets</Link>
+        <Link className={styles.sub} to='#'>Syndicate</Link>
+      </div>
+    </li>
+    <li>
+      <div>
+        <Link className={styles.main} to='#'>Account Login</Link>
+        <Link className={styles.sub} to='#'>Sub Menu</Link>
+      </div>
+    </li>
+    <li>
+      <div>
+        <Link className={styles.main} to='#'>Join Our Team</Link>
+        <Link className={styles.sub} to='#'>Sub Menu</Link>
+      </div>
+    </li>
+    <li>
+      <div>
+        <Link className={styles.main} to='#'>Contact Us</Link>
+        <Link className={styles.sub} to='#'>Sub Menu</Link>
+      </div>
+    </li>
+  </ul>
+)
+
+FooterMenu.displayName = 'FooterMenu'
+export default FooterMenu
