72b6d4d20ea92ef236ddb7b63ad7727fbdcd99aa
Gregory Varias <gregvarias@gmail.com>
2018-04-25 01:50:16 -0700

    style(layout): add max-width and margin to center all content

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,8 +5,10 @@ import Helmet from 'react-helmet'
 import Header from '../components/Header/Header'
 import Footer from '../components/Footer/Footer'
 
+import styles from './layout.module.less'
+
 const Layout = ({ children, data }) => (
-  <div>
+  <div className={styles.container}>
     <Helmet
       title={data.site.siteMetadata.title}
       meta={[
@@ -15,14 +17,7 @@ const Layout = ({ children, data }) => (
       ]}
     />
     <Header siteTitle={data.site.siteMetadata.title} />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
+    <div className={styles.mainContent}>
       {children()}
     </div>
     <Footer />
