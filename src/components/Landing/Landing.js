2c460e04425e99e8c3198c79b3d12b79854ad8ae
Gregory Varias <gregvarias@gmail.com>
2018-04-25 01:51:58 -0700

    feat(components): add Landing page layout

---
+++ src/components/Landing/Landing.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Image from './Image'
+import tempPhoto from './tempPhoto.jpeg'
+
+import styles from './landing.module.less'
+
+const Landing = () => (
+  <div className={styles.container}>
+    <div className={styles.hero}>
+      <Image src={tempPhoto} />
+    </div>
+    <div className={styles.group3}>
+      <Image src={tempPhoto} />
+      <Image src={tempPhoto} />
+      <Image src={tempPhoto} />
+    </div>
+  </div>
+)
+
+Landing.displayName = 'Landing'
+export default Landing
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,9 @@
 import React from 'react'
-import Link from 'gatsby-link'
+import Landing from '../components/Landing/Landing'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Landing />
   </div>
 )
 
