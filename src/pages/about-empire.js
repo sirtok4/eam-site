0c3bae3a36f457a8a639ea8e957082327a7b3d92
Gregory Varias <gregvarias@gmail.com>
2018-05-06 02:27:07 -0700

    feat(about): add another banner and standard content to about page

--- src/pages/about-empire.js
+++ src/pages/about-empire.js
@@ -3,14 +3,16 @@ import Link from 'gatsby-link'
 import Buttons from '../components/Buttons/Buttons'
 import Banner from '../components/Images/Banner/Banner'
 
-import tempPhoto from '../components/Landing/tempPhoto.jpeg'
+import banner1 from '../../assets/images/mfg-about_empire-1.png'
+import banner2 from '../../assets/images/mfg-about_empire-2.png'
 
 import styles from './about-empire.module.less'
 
 const AboutEmpire = () => (
   <div className={styles.container}>
     <Buttons />
-    <Banner src={tempPhoto} />
+    <Banner src={banner1} />
+    <div className={styles.callout}>We provide a wide range of investing, banking, financial advisory, and management services through our high net worth individual, corporate, and institutional client base.</div>
     <div className={styles.stdContent}>
       <div className={styles.left}>
         <div className={styles.section}>
@@ -29,7 +31,20 @@ const AboutEmpire = () => (
         </div>
       </div>
     </div>
-    <Banner src={tempPhoto} />
+    <Banner src={banner2} />
+    <div className={styles.callout}>At Empire, we are committed to a distinctive way of doing business and an unshakable emphasis on long-term client relationships.</div>
+    <div className={styles.stdContent}>
+      <div className={styles.left}>
+        <div className={styles.section}>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In maxime modi quo dignissimos at ea ipsa blanditiis quam optio saepe fuga sint ratione nisi excepturi necessitatibus, accusamus quisquam assumenda nemo ab adipisci laborum enim beatae. Cupiditate hic expedita labore, ad officia consectetur. Eveniet ipsa vitae dolorum ratione assumenda consectetur culpa fuga in, repellat ea est fugit cumque ipsam quia repellendus.</p>
+        </div>
+      </div>
+      <div className={styles.right}>
+        <div className={styles.section}>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum mollitia ab delectus? Ex aliquid qui quis provident consequuntur quaerat eius ipsam, impedit maxime ea incidunt maiores, enim quidem. Perspiciatis facilis pariatur ipsam odio amet maxime vero fuga placeat officia quo.</p>
+        </div>
+      </div>
+    </div>
   </div>
 )
 
