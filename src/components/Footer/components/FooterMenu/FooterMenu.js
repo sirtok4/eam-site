497bed31792e144bb40d999f4c128fc0a622072b
Gregory Varias <gregvarias@gmail.com>
2018-05-09 00:25:08 -0700

    style(footer): make markup more semantic and dry styles

--- src/components/Footer/components/FooterMenu/FooterMenu.js
+++ src/components/Footer/components/FooterMenu/FooterMenu.js
@@ -7,44 +7,44 @@ const FooterMenu = () => (
   <ul className={styles.container}>
     <li>
       <div>
-        <Link className={styles.main} to='#'>About Empire</Link>
+        <h6><Link className={styles.main} to='#'>About Empire</Link></h6>
         <Link className={styles.sub} to='#'>Our Philosophy</Link>
         <Link className={styles.sub} to='#'>Relationships Matter</Link>
       </div>
     </li>
     <li>
       <div>
-        <Link className={styles.main} to='#'>Brokerage</Link>
+        <h6><Link className={styles.main} to='#'>Brokerage</Link></h6>
         <Link className={styles.sub} to='#'>Transactional Brokerage</Link>
         <Link className={styles.sub} to='#'>Institutional Research, Sales &amp; Trading</Link>
       </div>
     </li>
     <li>
       <div>
-        <Link className={styles.main} to='#'>Asset Management</Link>
+        <h6><Link className={styles.main} to='#'>Asset Management</Link></h6>
         <Link className={styles.sub} to='#'>Financial Planning</Link>
       </div>
     </li>
     <li>
       <div>
-        <Link className={styles.main} to='#'>Investment Banking</Link>
+        <h6><Link className={styles.main} to='#'>Investment Banking</Link></h6>
         <Link className={styles.sub} to='#'>Capital Markets</Link>
         <Link className={styles.sub} to='#'>Syndicate</Link>
       </div>
     </li>
     <li>
       <div>
-        <Link className={styles.main} to='#'>Account Login</Link>
+        <h6><Link className={styles.main} to='#'>Account Login</Link></h6>
       </div>
     </li>
     <li>
       <div>
-        <Link className={styles.main} to='#'>Join Our Team</Link>
+        <h6><Link className={styles.main} to='#'>Join Our Team</Link></h6>
       </div>
     </li>
     <li>
       <div>
-        <Link className={styles.main} to='#'>Contact Us</Link>
+        <h6><Link className={styles.main} to='#'>Contact Us</Link></h6>
       </div>
     </li>
   </ul>
