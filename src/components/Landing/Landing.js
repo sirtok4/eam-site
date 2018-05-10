55ccd21948eedf2d7bf5d7192bca7687321efd53
Gregory Varias <gregvarias@gmail.com>
2018-05-10 01:13:45 -0700

    feat(landing): WIP: add images to landing page, need scaling fix

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import Image from '../Images/Image'
-import tempPhoto from './tempPhoto.jpeg'
+import photo1 from '../../../assets/images/mfg-homepage@2x.png'
+import photo2 from '../../../assets/images/mfg-brokerage-1.png'
+import photo3 from '../../../assets/images/mfg-asset_management.png'
+import photo4 from '../../../assets/images/mfg-investment_banking-1.png'
 
 import styles from './landing.module.less'
 
 const Landing = () => (
   <div className={styles.container}>
     <div className={styles.hero}>
-      <Image src={tempPhoto} />
+      <Image src={photo1} />
     </div>
     <div className={styles.group3}>
-      <Image src={tempPhoto} />
-      <Image src={tempPhoto} />
-      <Image src={tempPhoto} />
+      <Image src={photo2} />
+      <Image src={photo3} />
+      <Image src={photo4} />
     </div>
   </div>
 )
