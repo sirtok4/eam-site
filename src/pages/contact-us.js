3fe4c2140deccba3697f3aa61d32c9006b5e85b6
Gregory Varias <gregvarias@gmail.com>
2018-05-23 03:19:15 -0700

    feat(contact): add google map to contact us page

--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -43,6 +43,9 @@ const ContactUs = () => (
       </div>
       <div className={styles.right}>
         <h2>Our Office</h2>
+        <div className={styles.map}>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3024.5385267733996!2d-74.01589668459518!3d40.70615997933263!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25a173d1b444d%3A0x7775ebc6ec42bcd0!2sEmpire+Asset+Management!5e0!3m2!1sen!2sus!4v1527069949906" width="588" height="300" frameBorder="0" style={{ border: '0' }} allowFullScreen></iframe>
+        </div>
         <div className={styles.section}>
           <div className={styles.address}>
             <div>
