e84cbc8d92a4d46b98abf6a5a9b70e33fa554cd0
Gregory Varias <gregvarias@gmail.com>
2018-05-24 04:54:55 -0700

    feat(contactUs): add netlify form attributes for contact us page

--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -5,6 +5,11 @@ import banner1 from '../../assets/images/mfg-contact_us@2x.png'
 
 import styles from './contact-us.module.less'
 
+const handleReset = e => {
+  e.preventDefault()
+  document.getElementById('contact-form').reset()
+}
+
 const ContactUs = () => (
   <div className={styles.contactUsContainer}>
     <Banner src={banner1} caption='Build With Us.' />
@@ -13,33 +18,35 @@ const ContactUs = () => (
       <div className={styles.left}>
         <h2>Email Us</h2>
         <p>To find out if a relationship with Empire is right for you, please contact us today.</p>
-        <div className={styles.section}>
-          <div className={styles.field}>
-            <label htmlFor='name'>Full Name</label>
-            <input id='name' name='name' type='text' />
-          </div>
+        <form id='contact-form' name='contact' method='POST' data-netlify='true'>
+          <div className={styles.section}>
+            <div className={styles.field}>
+              <label htmlFor='name'>Full Name</label>
+              <input id='name' name='name' type='text' />
+            </div>
 
-          <div className={styles.field}>
-            <label htmlFor='company'>Company</label>
-            <input id='company' name='company' type='text' />
-          </div>
+            <div className={styles.field}>
+              <label htmlFor='company'>Company</label>
+              <input id='company' name='company' type='text' />
+            </div>
 
-          <div className={styles.field}>
-            <label htmlFor='email'>Email Address</label>
-            <input id='email' name='email' type='email' />
-          </div>
+            <div className={styles.field}>
+              <label htmlFor='email'>Email Address</label>
+              <input id='email' name='email' type='email' />
+            </div>
 
 
-          <div className={styles.field}>
-            <label htmlFor='questions-comments'>Questions / Comments</label>
-            <textarea id='questions-comments' name='questions-comments' />
-          </div>
+            <div className={styles.field}>
+              <label htmlFor='questions-comments'>Questions / Comments</label>
+              <textarea id='questions-comments' name='questions-comments' />
+            </div>
 
-          <div className={styles.field}>
-            <button className={styles.submit}>Submit</button>
-            <button className={styles.clear}>Clear</button>
+            <div className={styles.field}>
+              <button className={styles.submit} type='submit'>Submit</button>
+              <button className={styles.clear} onClick={handleReset}>Clear</button>
+            </div>
           </div>
-        </div>
+        </form>
       </div>
       <div className={styles.right}>
         <h2>Our Office</h2>
