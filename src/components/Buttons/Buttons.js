95a15ead8ddf68003bc3d9f544ace6a38cb3c876
Gregory Varias <gregvarias@gmail.com>
2018-06-04 23:58:59 -0700

    refactor(buttons): change nesting of Buttons component to work in all browsers

--- src/components/Buttons/Buttons.js
+++ src/components/Buttons/Buttons.js
@@ -4,10 +4,9 @@ import styles from './buttons.module.less'
 
 const Buttons = ({ x, y }) => (
   <div className={styles.container}>
-    <button className={styles.blue}><a href={x.location}>{x.label}</a></button>
-    <button className={styles.gray}><a href={y.location}>{y.label}</a></button>
+    <a href={x.location}><button className={styles.blue}>{x.label}</button></a>
+    <a href={y.location}><button className={styles.gray}>{y.label}</button></a>
   </div>
 )
 
-Buttons.displayName = 'Buttons'
 export default Buttons
