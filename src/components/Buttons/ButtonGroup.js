6f5dacb97a5ffce6cc8d76ce82413e7263bfa3bf
Gregory Varias <gregvarias@gmail.com>
2018-06-09 22:52:15 -0700

    feat(clientsRights): add button handlers that show selected page value

--- src/components/Buttons/ButtonGroup.js
+++ src/components/Buttons/ButtonGroup.js
@@ -4,10 +4,10 @@ import styles from './button-group.module.less'
 
 const Buttons = ({ a, b, x, y }) => (
   <div className={styles.container}>
-    <a href={a.location}><button className={styles.blue}>{a.label}</button></a>
-    <a href={b.location}><button className={styles.gray}>{b.label}</button></a>
-    <a href={x.location}><button className={styles.blue}>{x.label}</button></a>
-    <a href={y.location}><button className={styles.gray}>{y.label}</button></a>
+    <a><button onClick={a.onClick} value={a.value} className={styles.active}>{a.label}</button></a>
+    <a><button onClick={a.onClick} value={b.value} className={styles.inactive}>{b.label}</button></a>
+    <a><button onClick={a.onClick} value={x.value} className={styles.inactive}>{x.label}</button></a>
+    <a><button onClick={a.onClick} value={y.value} className={styles.inactive}>{y.label}</button></a>
   </div>
 )
 
--- src/pages/clients-rights.js
+++ src/pages/clients-rights.js
@@ -1,17 +1,41 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ButtonGroup from '../components/Buttons/ButtonGroup'
 
 import styles from './clients-rights.module.less'
 
-const ClientsRights = () => (
-  <div className={styles.clientsRightsContainer}>
-    <ButtonGroup
-      a={{ label: 'Clients Rigths & Protections', location: '' }}
-      b={{ label: 'Business Continuity Plan', location: '' }}
-      x={{ label: 'Privacy Policy', location: '' }}
-      y={{ label: '606 Reporting', location: '' }}
-    />
-  </div>
-)
+// Page values
+const CLIENTS_RIGHTS = 'clientsRights'
+const BUSINESS_CONTINUITY_PLAN = 'businessContinuityPlan'
+const PRIVACY_PLAN = 'privacyPlan'
+const REPORTING = '606reporting'
+
+class ClientsRights extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      currentPage: 'clientsRights'
+    }
+
+    this.handlePageChange = this.handlePageChange.bind(this)
+  }
+
+  handlePageChange (e) {
+    console.log('e', e.target.value)
+  }
+
+  render () {
+    return (
+      <div className={styles.clientsRightsContainer}>
+        <ButtonGroup
+          a={{ label: 'Clients Rigths & Protections', onClick: this.handlePageChange, value: CLIENTS_RIGHTS }}
+          b={{ label: 'Business Continuity Plan', onClick: this.handlePageChange, value: BUSINESS_CONTINUITY_PLAN }}
+          x={{ label: 'Privacy Policy', onClick: this.handlePageChange, value: PRIVACY_PLAN }}
+          y={{ label: '606 Reporting', onClick: this.handlePageChange, value: REPORTING }}
+        />
+      </div>
+    )
+  }
+}
 
 export default ClientsRights
