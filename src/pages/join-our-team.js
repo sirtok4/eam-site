adb57b7dba4984cb0da5a4494005a487e7f90d67
Gregory Varias <gregvarias@gmail.com>
2018-06-21 00:45:00 -0700

    feat(join): update content for join-us page

--- src/pages/join-our-team.js
+++ src/pages/join-our-team.js
@@ -10,58 +10,32 @@ const JoinOurTeam = () => (
     <Banner src={banner1} caption='Teamwork And Collaboration' />
 
     <div className={styles.callout}>
-      Every investment plan is customized based on a thorough assessment of each client's unique goals, needs and appetite for risk while remaining cognizant of the changing market environment.
+      Our team provides the highest level of personal counsel. If you are smart, hardworking, and nice, we would like to meet with you.
     </div>
 
     <div className={styles.stdContent}>
       <div className={styles.left}>
-        <h4>Customized Solutions</h4>
         <div className={styles.section}>
-          <p>Through our registered investment adviser affiliate, Empire Financial Management Company, we provide a full range of customized investment solutions leveraging the expertise of over 5,000 leading asset managers that control risk while maximizing potential for return.  Every investment plan is customized based on a thorough assessment of each client’s unique goals, needs and appetite for risk while remaining cognizant of the changing market environment.</p>
+          <h4>Clients Come First</h4>
+          <p>Most independent investment firms provide similar payouts. The real difference is the quality of the products and the level of service provided to your customers. At Empire, we recognize that servicing your clients is the most important thing you do, and servicing you is the most important thing we do.</p>
         </div>
         <div className={styles.section}>
-          <p>Our financial advisors are highly skilled in weighting of various asset categories and diversification in formulating a balanced investment plan to meet your investment objectives.</p>
+          <h4>Products &amp; Services</h4>
+          <p>Empire’s product offering is open architecture. Being part of our team gives you the latitude to select what is best for your client. Our in-house and direct relationships with leading equity/fixed income portfolio managers, alternative investments, Business Development Companies, Certified Financial Planning and Insurance Products allows you the confidence to meet any clients goals and needs.</p>
         </div>
         <div className={styles.section}>
-          <p>Our full range of options include:</p>
-          <ul>
-            <li>
-              <p>Separately Managed Accounts</p>
-            </li>
-            <li>
-              <p>Multi-Manager Accounts</p>
-            </li>
-            <li>
-              <p>Unified Managed Accounts</p>
-            </li>
-            <li>
-              <p>Mutual Fund Strategies</p>
-            </li>
-            <li>
-              <p>ETF Strategies</p>
-            </li>
-            <li>
-              <p>Alternative Investments</p>
-            </li>
-          </ul>
+          <h4>Build With Us</h4>
+          <p>As an advisor, you're building value for your clients and practice every day. At Empire, we recognize that you have worked tirelessly, and that’s why we offer transparent payouts and pricing to improve your profitability. Empire will provide the scale and resources that allows you to grow your business with better client experiences.</p>
         </div>
       </div>
       <div className={styles.right}>
-        <h4>Identifying Your Goals</h4>
         <div className={styles.section}>
-          <p>Our investment process begins with an analysis of your needs and goals.  A profile is developed that highlights your investment objectives, tolerance for risk and investment time horizon and then used as the foundation for a long-range investment strategy.  One established, we will work together with you to define an investment plan tailored to your specific situation.</p>
+          <h4>Technology</h4>
+          <p>Streamline your business and maximize building your client relationship through operational efficiencies. We are fully digital, so both you and your clients will begin realizing the benefits of a paperless office immediately. Your clients will be able to execute new account documents, perform a wire request, or execute an account transfer from any mobile device instantly.</p>
         </div>
         <div className={styles.section}>
-          <h4>Developing Your Investment Strategy</h4>
-          <p>Our financial advisors will assist you in weighting of various asset categories (i.e., stocks, bonds, cash) as well as diversification of holdings to reduce risk and increase the security of your portfolio based on your personalized risk profile.</p>
-        </div>
-        <div className={styles.section}>
-          <h4>Your Investment Plan</h4>
-          <p>Through a careful evaluation and due diligence process that screens over 5,000 investment management firms our financial advisors are able to present our clients with some of the best investment managers who specialize in a broad range of asset classes.  The selection of appropriate managers for your customized investment plan allows us offer you a blended management and allocation strategy that best suits your needs.</p>
-        </div>
-        <div className={styles.section}>
-          <h4>Monitoring Your Portfolio</h4>
-          <p>Following your investment, your portfolio will be monitored on an ongoing basis to ensure that it remains on track to achieve the investment goals and objectives.  Additionally, our financial advisors stand ready to assist when market conditions or other factors may cause the allocations to deviate from the original plan.</p>
+          <h4>Transition Assistance</h4>
+          <p>At Empire, you will find plenty of support. When joining our team, you will be welcomed by an experienced, dedicated conversion consultant to walk you through each step. We have become proficient in moving your book of business with the least impact to your clients. Because Empire is fully electronic, your clients will not face an overwhelming physical burden of new account agreements and transfer forms. Instead, they will receive a single email, which they can digitally execute from any mobile device in the world and start their journey to a better client relationship. Build with us!</p>
         </div>
       </div>
     </div>
