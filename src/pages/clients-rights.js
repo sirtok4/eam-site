bacc0b6e2118e40adfda7df057ac43d47e9d4450
Empire Asset Management <40455360+eam18@users.noreply.github.com>
2019-09-25 17:28:42 -0400

    Update clients-rights.js
    The word "Rights" was spelt incorrectly in the page nav.

--- src/pages/clients-rights.js
+++ src/pages/clients-rights.js
@@ -7,7 +7,7 @@ import styles from './clients-rights.module.less'
 const ClientsRights = () => (
   <div className={styles.clientsRightsContainer}>
     <ButtonGroup
-      a={{ label: 'Clients Rigths & Protections', to: '/clients-rights', isActive: true }}
+      a={{ label: 'Clients Rights & Protections', to: '/clients-rights', isActive: true }}
       b={{ label: 'Business Continuity Plan', to: '/business-continuity-plan', isActive: false }}
       x={{ label: 'Privacy Policy', to: '/privacy-policy', isActive: false }}
       y={{ label: '606 Reporting', to: '/reporting', isActive: false }}
