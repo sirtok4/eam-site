61745cfeda15e009f5a40c617b35a545ee8753ee
Empire Asset Management <40455360+eam18@users.noreply.github.com>
2020-06-23 11:17:31 -0400

    Update Reporting.js

--- src/components/ClientsRights/Reporting.js
+++ src/components/ClientsRights/Reporting.js
@@ -38,7 +38,7 @@ const Reporting = () => (
      </div>
     <div className={styles.section}>
       <a
-        href="https://github.com/gregRV/eam-site/blob/master/Empire%20CRS%20BD%20(06-01-20)%20(003).pdf"
+        href="https://github.com/gregRV/eam-site/blob/master/Empire%20CRS%20BD%20(06-01-20)%20(003).pdf?raw=true"
         target="_blank" type="application/pdf"
       >
         <p>Empire Asset Management Company</p>
