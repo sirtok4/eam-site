145f74e0175346ec4e9ef13e263ef6cfd615ba6e
Empire Asset Management <40455360+eam18@users.noreply.github.com>
2020-06-23 10:09:53 -0400

    Update FinePrint.js
    Updated Copyright to 2020

--- src/components/Footer/components/FinePrint/FinePrint.js
+++ src/components/Footer/components/FinePrint/FinePrint.js
@@ -5,7 +5,7 @@ import styles from './fine-print.module.less'
 
 const FinePrint = () => (
   <div className={styles.container}>
-    <p>&copy; 2017 Empire Asset Management Company | 29 Broadway, 12th Floor, New York, NY 10006 | 888-417-8050 | <a href='mailto:info@empiream.com'>info@empiream.com</a></p>
+    <p>&copy; 2020 Empire Asset Management Company | 29 Broadway, 12th Floor, New York, NY 10006 | 888-417-8050 | <a href='mailto:info@empiream.com'>info@empiream.com</a></p>
     <p>Member FINRA / SIPC | <a href='https://brokercheck.finra.org/' target='_blank'>FINRA's BrokerCheck</a> | <Link to='/clients-rights'>Client's Rights &amp; Protections</Link></p>
   </div>
 )
