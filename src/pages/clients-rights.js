3e2a6b3c6bba94a810629b9e921200a061e4214c
Empire Asset Management <40455360+eam18@users.noreply.github.com>
2020-06-23 09:31:22 -0400

    Update clients-rights.js
    Updated 606 Reporting to '606 Reports / Form CRS'

--- src/pages/clients-rights.js
+++ src/pages/clients-rights.js
@@ -10,7 +10,7 @@ const ClientsRights = () => (
       a={{ label: 'Clients Rights & Protections', to: '/clients-rights', isActive: true }}
       b={{ label: 'Business Continuity Plan', to: '/business-continuity-plan', isActive: false }}
       x={{ label: 'Privacy Policy', to: '/privacy-policy', isActive: false }}
-      y={{ label: '606 Reporting', to: '/reporting', isActive: false }}
+      y={{ label: '606 Reports / Form CRS', to: '/reporting', isActive: false }}
     />
     <div className={styles.stdContent}>
       <Content />
