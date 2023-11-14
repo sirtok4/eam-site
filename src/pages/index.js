36334b58ecddf5a676f97cd06edad245aca634e4
Empire Asset Management <40455360+eam18@users.noreply.github.com>
2023-11-14 12:08:06 -0500

    Update index.js
    Removing tag manager

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,10 +6,4 @@ const IndexPage = () => (
     <Landing />
   </div>
 )
-
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src=https://www.googletagmanager.com/ns.html?id=GTM-MPBSVMVR
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
-
 export default IndexPage
