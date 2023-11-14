49ec4ecbd1cd1b181046dde1f50d3026d90e83c8
Empire Asset Management <40455360+eam18@users.noreply.github.com>
2023-11-14 11:35:40 -0500

    Update index.js
    Adding Google Tag Manager body script per Gregg Zeoli

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,4 +7,9 @@ const IndexPage = () => (
   </div>
 )
 
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src=https://www.googletagmanager.com/ns.html?id=GTM-MPBSVMVR
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
 export default IndexPage
