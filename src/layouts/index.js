d6116e886ad86c83a0ec58dee76495998043c231
Jean Hoffmann <55813212+sirtok4@users.noreply.github.com>
2023-11-15 21:38:39 +0000

    google tag integration

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
@@ -7,21 +7,38 @@ import Footer from '../components/Footer/Footer'
 
 import styles from './layout.module.less'
 
-const Layout = ({ children, data }) => (
-  <div className={styles.mainContainer}>
+import TagManager from 'react-gtm-module';
+
+const Layout = ({ children, data }) => {
+  useEffect(() => {
+    TagManager.initialize({
+      gtmId: 'GTM-MPBSVMVR',
+    })
+  }, []);
+  function handleGoogleTagClick() {
+    TagManager.dataLayer({
+      dataLayer: {
+        event: 'button_click',
+        buttonName: 'Example Button',
+      },
+    });
+  }
+  return <div className={styles.mainContainer}>
     <Helmet
       title='Empire Asset Management'
     >
       <meta name='viewport' content='width=device-width, initial-scale=1.0, shrink-to-fit=no' />
+      
       <script src='https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/6.26.0/polyfill.min.js' />
     </Helmet>
     <div className={styles.mainContent}>
+      <button onClick={handleGoogleTagClick}>Test Button GoogleTag Click me</button>
       <Header siteTitle='Empire Asset Management' />
       {children()}
     </div>
     <Footer />
   </div>
-)
+}
 
 Layout.propTypes = {
   children: PropTypes.func,
